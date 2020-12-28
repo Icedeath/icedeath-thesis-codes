34f006d614611b0f842c86b7d3d435c3fe1c282f
Icedeath <2724432@qq.com>
2020-12-28 10:41:11 +0800

    20201228

---
+++ Chap_4/SAE/signal_generation/make_train.m
@@ -0,0 +1,49 @@
+clear all
+clc
+load ../dataset/data_fe
+
+n_tr=5;
+n_te=1000;
+
+tr1 = zeros(N_samples,25);
+tr2 = zeros(N_samples,25);
+tr3 = zeros(N_samples,25);
+tr4 = zeros(N_samples,25);
+tr5 = zeros(N_samples,25);
+tr6 = zeros(N_samples,25);
+tr7 = zeros(N_samples,25);
+tr8 = zeros(N_samples,25);
+
+try1 = zeros(N_samples,1);
+try2 = ones(N_samples,1);
+try3 = ones(N_samples,1)*2;
+try4 = ones(N_samples,1)*3;
+try5 = ones(N_samples,1)*4;
+try6 = ones(N_samples,1)*5;
+try7 = ones(N_samples,1)*6;
+try8 = ones(N_samples,1)*7;
+train_y=[try1(1:n_tr,:);try2(1:n_tr,:);try3(1:n_tr,:);try4(1:n_tr,:);try5(1:n_tr,:)...
+    ;try6(1:n_tr,:);try7(1:n_tr,:);try8(1:n_tr,:)];
+test_y=[try1(N_samples-n_te+1:N_samples,:);try2(N_samples-n_te+1:N_samples,:);try3(N_samples-n_te+1:N_samples,:);try4(N_samples-n_te+1:N_samples,:);try5(N_samples-n_te+1:N_samples,:)...
+    ;try6(N_samples-n_te+1:N_samples,:);try7(N_samples-n_te+1:N_samples,:);try8(N_samples-n_te+1:N_samples,:)];
+for snr = snr_min:2:snr_max
+    trx1 = mode1((snr-snr_min)/2*N_samples+1:(snr-snr_min)/2*N_samples+N_samples,:);
+    trx2 = mode2((snr-snr_min)/2*N_samples+1:(snr-snr_min)/2*N_samples+N_samples,:);
+    trx3 = mode3((snr-snr_min)/2*N_samples+1:(snr-snr_min)/2*N_samples+N_samples,:);
+    trx4 = mode4((snr-snr_min)/2*N_samples+1:(snr-snr_min)/2*N_samples+N_samples,:);
+    trx5 = mode5((snr-snr_min)/2*N_samples+1:(snr-snr_min)/2*N_samples+N_samples,:);
+    trx6 = mode6((snr-snr_min)/2*N_samples+1:(snr-snr_min)/2*N_samples+N_samples,:);
+    trx7 = mode7((snr-snr_min)/2*N_samples+1:(snr-snr_min)/2*N_samples+N_samples,:);
+    trx8 = mode8((snr-snr_min)/2*N_samples+1:(snr-snr_min)/2*N_samples+N_samples,:);
+    train_x=[trx1(1:n_tr,:);trx2(1:n_tr,:);trx3(1:n_tr,:);trx4(1:n_tr,:);trx5(1:n_tr,:)...
+    ;trx6(1:n_tr,:);trx7(1:n_tr,:);trx8(1:n_tr,:)];
+    test_x=[trx1(N_samples-n_te+1:N_samples,:);trx2(N_samples-n_te+1:N_samples,:);trx3(N_samples-n_te+1:N_samples,:);trx4(N_samples-n_te+1:N_samples,:);trx5(N_samples-n_te+1:N_samples,:)...
+    ;trx6(N_samples-n_te+1:N_samples,:);trx7(N_samples-n_te+1:N_samples,:);trx8(N_samples-n_te+1:N_samples,:)];
+    a=strcat('Saving data_fe_',num2str(snr),'.mat...');
+    disp(a)
+    for i = 1:25
+        train_x(:,i)=train_x(:,i)/mean(train_x(:,i))-1;
+        test_x(:,i)=test_x(:,i)/mean(test_x(:,i))-1;
+    end
+    save(strcat('../dataset/data_fe_',num2str(snr)),'train_x','train_y','test_x','test_y', 'N_samples','snr_max','snr_min','N_code')
+end
